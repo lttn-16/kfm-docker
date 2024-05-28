7b4413cb32f3bb97d8edc42c77e30556371b8696
nhile <lttn.16@gmail.com>
2024-05-28 21:19:45 +0700

    (feat) update FE, fix port

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -12,11 +12,14 @@ import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { Alert } from '@mui/material';
+import { useState } from 'react';
 
 const defaultTheme = createTheme();
 
 export default function Login() {
   const navigate = useNavigate();
+  const [error, setError] = useState('');
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -36,34 +39,42 @@ export default function Login() {
           },
         }
       );
-      if (response.data) {
-        navigate("/dashboard");
+      if (response.data.statusCode === 200) {
+        localStorage.setItem("authToken", "hello")
+        navigate("/welcome");
       }
     } catch (error) {
       console.error("Error:", error);
+      setError(error.response.data.message)
     }
   };
     
 
   return (
     <ThemeProvider theme={defaultTheme}>
+      {error && <Alert severity="error">{error}</Alert>}
       <Container component="main" maxWidth="xs">
         <CssBaseline />
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
             <TextField
               margin="normal"
               required
