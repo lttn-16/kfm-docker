e8ac1f2ec2e2073c0c48ca2a1060399ce4357653
nhile <lttn.16@gmail.com>
2024-06-02 16:37:23 +0700

    config traefik

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:3000"
+  origin: ["http://localhost:3000", "http://ditre.localhost", "http://bangphongthan.localhost"]
 };
 
 app.use(cors(corsOptions));
